9ff70701a039f8d5b0ea1b7a4c1244a4abbc04a7
Bpratik2912 <bpratik76@gmail.com>
2022-07-25 23:22:49 +0530

    changes applied

--- main.py
+++ main.py
@@ -22,48 +22,20 @@ print("1) RGP : Residential General Purpose (UP to and including 15 KW)\n2) GLP 
       "11) HTMD-2 : High Tension Water and Sewage Pumping Stations run by AMC\n12) HTMD-3 : High Tension Maximum Demand Temporary Supply\n"
       "13) EV: HT Electric Vehicle Charging Station\n14)HTMD : Metro Traction\n")
 
-# method = {1 : SelectRgp.last(), 2 : SelectGlp.glp(), 3 : SelectNonGlp.non_rgp(), 4 : SelectLtp.ltp(), 5 : SelectLtmdOne.ltmd_one(), 6 : SelectLtmdTwo.ltmd_two(), 7 : SelectSl.sl(), 8 : SelectLev.lev(), 9 : SelectLev.lev(), 10 : SelectHtmdOne.htmd_one(),
-# 11 : SelectHtmdTwo.htmd_two(), 12 : SelectHtmdThree.htmd_three(), 13 : SelectEv.htmd_ev(),14 : SelectHtmd.htmd()}
-# method = {1:'pratik',2:'vatsal',3:'paras' }
+method = {1 : SelectRgp.last, 2 : SelectGlp.glp, 3 : SelectNonGlp.non_rgp, 4 : SelectLtp.ltp, 5 : SelectLtmdOne.ltmd_one, 6 : SelectLtmdTwo.ltmd_two, 7 : SelectSl.sl, 8 : SelectLev.lev, 9 : SelectLev.lev, 10 : SelectHtmdOne.htmd_one,
+11 : SelectHtmdTwo.htmd_two, 12 : SelectHtmdThree.htmd_three, 13 : SelectEv.htmd_ev,14 : SelectHtmd.htmd}
 
-# print(method[1])
 
-# for i in method.keys():
-#     print(method[i])
+def calculate():
+    i = int(input("enter a method number:"))
+    try:
+        if i in method.keys():
+            p = method[i]
+            return p()
+        else:
+            raise ValueError
+    except ValueError:
+        return "invalid input!!!!"
 
-i = int(input("enter a method number:"))
+calculate()
 
-try:
-    if i == 1:
-        SelectRgp.last()
-    elif i == 2:
-        SelectGlp.glp()
-    elif i == 3:
-        SelectNonGlp.non_rgp()
-    elif i == 4:
-        SelectLtp.ltp()
-    elif i == 5:
-        SelectLtmdOne.ltmd_one()
-    elif i == 6:
-        SelectLtmdTwo.ltmd_two()
-    elif i == 7:
-        SelectSl.sl()
-    elif i == 8:
-        SelectLev.lev()
-    elif i == 9:
-        SelectTmp.tmp()
-    elif i == 10:
-        SelectHtmdOne.htmd_one()
-    elif i == 11:
-        SelectHtmdTwo.htmd_two()
-    elif i == 12:
-        SelectHtmdThree.htmd_three()
-    elif i == 13:
-        SelectEv.htmd_ev()
-    elif i == 14:
-        SelectHtmd.htmd()
-    else:
-        raise ValueError
-        
-except ValueError:
-    print("invalid input!!!!!!")
