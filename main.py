b4deb53f5d5ecab381c19a5fbf3c9257bac67b90
Bpratik2912 <bpratik76@gmail.com>
2022-07-26 12:20:26 +0530

    apply flage8

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-from ast import NotIn
 from methods.one_RGP import SelectRgp
 from methods.two_GLP import SelectGlp
 from methods.three_Non_RGP import SelectNonGlp
@@ -15,30 +14,38 @@ from methods.thirteen_EV import SelectEv
 from methods.fourteen_HTMD import SelectHtmd
 
 print("Torrent Power electricity billing")
-print("1) RGP : Residential General Purpose (UP to and including 15 KW)\n2) GLP :For Hospitals,Schools & Religious Purpose\n"
-      "3) Non-RGP : Low Tension Service for Commercial and Industrial Purpose\n4) LTP (AG) : Agriculture Service\n"
-      "5) LTMD-1 : Low Tension Maximum Demand for Residential Purpose\n6) LTMD-2 : Low Tension Maximum Demand for other than residential purpose\n"
-      "7) SL : Low Tension Tension Street Light Service\n8) LEV : LT- Electric Vehicle Charging Stations\n"
-      "9) TMP : Low Tension Temporary Supply\n10) HTMD-1 : High Tension Maximum Demand\n"
-      "11) HTMD-2 : High Tension Water and Sewage Pumping Stations run by AMC\n12) HTMD-3 : High Tension Maximum Demand Temporary Supply\n"
-      "13) EV: HT Electric Vehicle Charging Station\n14)HTMD : Metro Traction\n")
+print("1) RGP : Residential General Purpose (UP to and including 15 KW)\n"
+      "2) GLP :For Hospitals,Schools & Religious Purpose\n"
+      "3) Non-RGP : Low Tension Service for Commercial and Industrial Purpose\n"
+      "4) LTP (AG) : Agriculture Service\n"
+      "5) LTMD-1 : Low Tension Maximum Demand for Residential Purpose\n"
+      "6) LTMD-2 : Low Tension Maximum Demand for other than residential purpose\n"
+      "7) SL : Low Tension Tension Street Light Service\n"
+      "8) LEV : LT- Electric Vehicle Charging Stations\n"
+      "9) TMP : Low Tension Temporary Supply\n"
+      "10) HTMD-1 : High Tension Maximum Demand\n"
+      "11) HTMD-2 : High Tension Water and Sewage Pumping Stations run by AMC\n"
+      "12) HTMD-3 : High Tension Maximum Demand Temporary Supply\n"
+      "13) EV: HT Electric Vehicle Charging Station\n"
+      "14)HTMD : Metro Traction\n")
 
-method = {1 : SelectRgp.last, 2 : SelectGlp.glp, 3 : SelectNonGlp.non_rgp, 4 : SelectLtp.ltp, 5 : SelectLtmdOne.ltmd_one, 6 : SelectLtmdTwo.ltmd_two, 7 : SelectSl.sl, 8 : SelectLev.lev, 9 : SelectLev.lev, 10 : SelectHtmdOne.htmd_one,
-11 : SelectHtmdTwo.htmd_two, 12 : SelectHtmdThree.htmd_three, 13 : SelectEv.htmd_ev,14 : SelectHtmd.htmd}
+method = {1: SelectRgp.last, 2: SelectGlp.glp, 3: SelectNonGlp.non_rgp,
+          4: SelectLtp.ltp, 5: SelectLtmdOne.ltmd_one, 6: SelectLtmdTwo.ltmd_two,
+          7: SelectSl.sl, 8: SelectLev.lev, 9: SelectTmp.tmp, 10: SelectHtmdOne.htmd_one,
+          11: SelectHtmdTwo.htmd_two, 12: SelectHtmdThree.htmd_three,
+          13: SelectEv.htmd_ev, 14: SelectHtmd.htmd}
 
 
 def calculate():
-    
     try:
         i = int(input("enter a method number:"))
         if i in method.keys():
-            p = method[i]
-            return p()
-        elif not i in  method.key():
+            return method[i]()
+        else:
             raise ValueError
 
     except ValueError:
-        return "invalid input!!!!"
+        print("invalid input!!!!")
+
 
 calculate()
-
